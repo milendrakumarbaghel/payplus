62437d2a67580f4425402e561ce881503273d658
milendrakumarbaghel <baghelmilendrakumar@gmail.com>
2024-12-07 23:17:21 +0530

    added signup endpoint/rout

--- backend/api/user.js
+++ backend/api/user.js
@@ -19,7 +19,7 @@ router.post('/signup', async (req, res)=> {
     const body = req.body;
     const {success} = signupSchema.safeParse(req.body);
     if(!success) {
-        return res.json({
+        return res.status(411).json({
             message: "Email already taken / Incorrect inputs"
         })
     }
@@ -51,5 +51,34 @@ const signinSchema = zod.object({
     password: zod.string()
 })
 
+router.post('/signin', async (req, res) => {
+    const body = req.body;
+    const {success} = signinSchema.safeParse(req.body);
+    if(!success) {
+        return req.status(411).json({
+            message: "Incorrect inputs"
+        })
+    }
+
+    const user = await User.findOne({
+        username: body.username,
+        password: body.password
+    })
+
+    if(user) {
+        const token = jwt.sign({
+            userId: user._id,
+        }, JWT_SECRET);
+
+        res.json({
+            token: token
+        })
+        return;
+    }
+
+    res.status(411).json({
+        message: "Error while logging in"
+    })
+})
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-const rootRouter = require('./api/routes');
 require('dotenv').config();
-const app = express();
-
 app.use(cors());
 app.use(express.json);
-app.use("/api/v1", rootRouter);
+const rootRouter = require('./api/routes');
+const app = express();
 
+app.use("/api/v1", rootRouter);
 app.listen(process.env.PORT || 8000);
