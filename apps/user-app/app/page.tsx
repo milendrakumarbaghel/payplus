a850bcb29c548d78db4dfc35daeea7bb55897650
milendrakumarbaghel <baghelmilendrakumar@gmail.com>
2025-05-02 11:43:20 +0530

    exporting prisma client via db package

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,6 +1,8 @@
 import Image, { type ImageProps } from "next/image";
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
+import { PrismaClient } from "@repo/db/client";
+const client = new PrismaClient();
 
 export default function Home() {
   return (
